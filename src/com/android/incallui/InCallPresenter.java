7640f5425e9dc462e22d96ce1600267e6bd1766c
Lars Greiss <kufikugel@googlemail.com>
2014-01-11 22:17:53 +0100

    InCallUI: add decline and answer  incoming call action on notifications
    Add decline and answer call action on notification if an call is incoming. Especially
    usefull for call in background feature to have the ability to directly
    decline or answer a incoming call. But as well usefull to decline/answer a second call
    during one is incomming.
    
    PS:
    - found a better way for the answer button
    
    PS:
    - fix keep flip action alive
    - fix collapse notification shade when answering call
    - answer call before starting the UI to do not have
      shown the incall screen for a brief second
    
    PS:
    - fix wrong user on sending close system dialogs
    
    Change-Id: I92d8f5a2de74f9877a61c4da4c3eaf2e25230895
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	AndroidManifest.xml
    	src/com/android/incallui/InCallPresenter.java

--- src/com/android/incallui/InCallPresenter.java
+++ src/com/android/incallui/InCallPresenter.java
@@ -274,9 +274,11 @@ public class InCallPresenter implements CallList.Listener {
 
         // Disable notification shade and soft navigation buttons
         // on new incoming call as long it is no background call
-        if (newState.isIncoming() && !mCallUiInBackground) {
-            CallCommandClient.getInstance().setSystemBarNavigationEnabled(false);
-            if(mAccelerometerListener != null){
+        if (newState.isIncoming()) {
+            if (!mCallUiInBackground) {
+                CallCommandClient.getInstance().setSystemBarNavigationEnabled(false);
+            }
+            if (mAccelerometerListener != null) {
                 mAccelerometerListener.enableSensor(true);
             }
         }
@@ -682,6 +684,18 @@ public class InCallPresenter implements CallList.Listener {
 
         mStatusBarNotifier.updateNotificationAndLaunchIncomingCallUi(
                 inCallState, mCallList, mCallUiInBackground);
+    }
+
+    /**
+     * Starts the incoming call Ui immediately used by the incoming call
+     * notification sent from framework's notification mechanism
+     */
+    public void startIncomingCallUi() {
+        // Update the notification and UI this time with fullscreen intent
+        // First cancel the actual notification and then update
+        mStatusBarNotifier.cancelInCall();
+        mStatusBarNotifier.updateNotificationAndLaunchIncomingCallUi(
+                InCallState.INCALL, mCallList, false);
     }
 
     /**
